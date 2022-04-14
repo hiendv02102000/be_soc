ddc3ff226ef0805e54b766df62e77637925fe0f2
KinBaos <letrungkien612000@gmail.com>
2022-04-15 00:11:59 +0700

    BE0063

--- internal/pkg/domain/domain_model/entity/chapters.go
+++ internal/pkg/domain/domain_model/entity/chapters.go
@@ -5,7 +5,5 @@ type Chapters struct {
 	Title      string `gorm:"column:title;"`
 	ContentUrl string `gorm:"column:content_url"`
 	NovelsID   int    `gorm:"column:novels_id"`
-	CommentsID int    `gorm:"column:comments_id"`
-
 	BaseModel
 }
