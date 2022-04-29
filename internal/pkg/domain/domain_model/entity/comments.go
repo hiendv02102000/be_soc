3f6086b0cc639a0ba6c8c17617e241205c971961
Your Name <hn5308434@gmail.com>
2022-04-29 23:12:54 +0700

    Update BE014-2

--- internal/pkg/domain/domain_model/entity/comments.go
+++ internal/pkg/domain/domain_model/entity/comments.go
@@ -5,5 +5,6 @@ type Comments struct {
 	ID              int    `gorm:"column:id;primary_key;auto_increment;not null"`
 	CommentsContent string `gorm:"column:cmt_content"`
 	UsersId         int    `gorm:"column:users_id"`
+	ChapterID       int    `gorm:"column:chapter_id"`
 	BaseModel
 }
