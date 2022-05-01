0c89c58e6ed5f6095fb91ab54e87c98a5ab4f1ab
Your Name <hn5308434@gmail.com>
2022-05-01 20:25:49 +0700

    Update BE005-4

--- graph/query/get_user_profile_query.go
+++ graph/query/get_user_profile_query.go
@@ -41,14 +41,19 @@ func GetUserProfileQuery(containerRepo map[string]interface{}) *graphql.Field {
 			})
 			fmt.Println(userNovel)
 			novels := make([]map[string]interface{}, 0)
-			for _, n := range userNovel {
-
+			for i := 0; i < len(novels); i++ {
+				n, err1 := novelRepo.FirstNovel(entity.Novels{
+					UsersID: userNovel[i].UsersID,
+				})
+				if err1 != nil {
+					return
+				}
 				fmt.Println(n)
 				novel := map[string]interface{}{
-					"id":        n.ID,
-					"name":      n.Name,
-					"image_url": n.ImageUrl,
-					"view":      n.View,
+					"id":      n.ID,
+					"name":    n.Name,
+					"img_url": n.ImageUrl,
+					"view":    n.View,
 				}
 				novels = append(novels, novel)
 			}
@@ -58,7 +63,7 @@ func GetUserProfileQuery(containerRepo map[string]interface{}) *graphql.Field {
 			}
 			result = map[string]interface{}{
 
-				"ID":         UserProfile.ID,
+				"id":         UserProfile.ID,
 				"first_name": UserProfile.FirstName,
 				"last_name":  UserProfile.LastName,
 				"username":   UserProfile.Username,
