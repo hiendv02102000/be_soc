6d9189d8af7f46c9b594b27aa4f251d445e0328c
Your Name <hn5308434@gmail.com>
2022-05-01 20:39:04 +0700

    Update BE005-5

--- pkg/infrastucture/schema/query.go
+++ pkg/infrastucture/schema/query.go
@@ -19,10 +19,9 @@ func GetClientQuery(containerRepo map[string]interface{}) *graphql.Object {
 	return graphql.NewObject(graphql.ObjectConfig{
 		Name: "query",
 		Fields: graphql.Fields{
-			"get_novel_list":     query.GetNovelListQuery(containerRepo),
-			"get_user_profile":   query.GetUserProfileQuery(containerRepo),
-			"get_chapter_info":   query.GetChapterInfoQuery(containerRepo),
-			"get_all_categories": query.GetAllCategories(containerRepo),
+			"get_novel_list":   query.GetNovelListQuery(containerRepo),
+			"get_user_profile": query.GetUserProfileQuery(containerRepo),
+			"get_chapter_info": query.GetChapterInfoQuery(containerRepo),
 		},
 	})
 }
