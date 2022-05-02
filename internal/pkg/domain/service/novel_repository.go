6e29cd08b2c6edf359469fda2fb4b0fc3073fd69
Your Name <hn5308434@gmail.com>
2022-05-02 18:10:43 +0700

    UpdateBE014

--- internal/pkg/domain/service/novel_repository.go
+++ internal/pkg/domain/service/novel_repository.go
@@ -1,6 +1,8 @@
 package service
 
-import "be_soc/internal/pkg/domain/domain_model/entity"
+import (
+	"be_soc/internal/pkg/domain/domain_model/entity"
+)
 
 type NovelRepositoryInterface interface {
 	FindNovelList(condition entity.Novels) (novels []entity.Novels, err error)
@@ -8,4 +10,5 @@ type NovelRepositoryInterface interface {
 	CreateNovel(entity.Novels) (entity.Novels, error)
 	UpdateNovel(novel, oldNovel entity.Novels) error
 	DeleteNovel(novel entity.Novels) error
+	UpdateView(query string) error
 }
