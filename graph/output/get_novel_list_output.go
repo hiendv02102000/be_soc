df66d0160b11ce1159666ca9557821ac13ddc5c3
hiendv02102000 <dovanhien021000@gmail.com>
2022-05-11 01:42:57 +0700

    123

--- graph/output/get_novel_list_output.go
+++ graph/output/get_novel_list_output.go
@@ -43,7 +43,7 @@ func NovelListOutput() *graphql.Object {
 												},
 											},
 										},
-									)},
+									)}, //
 								},
 								"categories": &graphql.Field{
 									Type: &graphql.List{OfType: graphql.NewObject(
